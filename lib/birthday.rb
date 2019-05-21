dbe82cf1b5a6b31d927139e5ecbcf6219411d160
Trevahr Robertson <trevahr@yahoo.com>
2019-05-21 13:22:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,7 @@ end
 def age_appropriate_birthday(birthday_kids)
 
   birthday_kids.each do |kids_name, age|
+<<<<<<< HEAD
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
@@ -26,3 +27,13 @@ def age_appropriate_birthday(birthday_kids)
 
 end
 #done
+=======
+    if age < 27
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+    if age >= 27
+      
+  end
+
+end
+>>>>>>> 928f4286f6c6b9d78837f7ae5e644912c8145ab0
